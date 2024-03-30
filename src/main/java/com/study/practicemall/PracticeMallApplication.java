09570ebab2e9cf24affbb119251d6e05ab6972a0
shinmirim <148671752+devVarier@users.noreply.github.com>
2024-03-30 14:20:10 +0900

    2024.3.30 프로젝트세팅

---
+++ src/main/java/com/study/practicemall/PracticeMallApplication.java
@@ -0,0 +1,14 @@
+package com.study.practicemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMallApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/practicemall/PracticeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.practicemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
