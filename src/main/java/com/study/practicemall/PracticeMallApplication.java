60c16da03c9b8b663dae63b7528eeb504916e898
shinmirim <148671752+devVarier@users.noreply.github.com>
2024-03-31 11:26:16 +0900

    2024.3.30 프로젝트세팅

--- src/main/java/com/study/practicemall/PracticeMallApplication.java
+++ src/main/java/com/study/practicemall/PracticeMallApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeMallApplication {
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeMallApplication.class, args);
 	}
