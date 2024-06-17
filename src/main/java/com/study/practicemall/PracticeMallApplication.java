fabb6a52a0779aa1401034995fabda2938d40a68
devVarier <148671752+devVarier@users.noreply.github.com>
2024-06-17 23:56:12 +0900

    dd

--- src/main/java/com/study/practicemall/PracticeMallApplication.java
+++ src/main/java/com/study/practicemall/PracticeMallApplication.java
@@ -1,11 +1,10 @@
 package com.study.practicemall;
-/**/
+/*신미림*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeMallApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeMallApplication.class, args);
 	}
